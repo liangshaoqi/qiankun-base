e0b91c30eb8c7fb93572be3175303d2c48f48aba
Shaoqi Liang <918872153@qq.com>
2024-06-04 09:54:26 +0800

    feat 微前端传参示例

---
+++ src/layout/components/KeepAliveOutlet/index.tsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+
+interface Props {
+  
+}
+
+const ComponentName: React.FC<Props> = (props) => {
+
+  const {} = props
+
+  const [] = useState()
+
+  useEffect(() => {}, [])
+
+  return (
+    <div>
+      组件模版
+    </div>
+  );
+};
+
+export default ComponentName;
--- src/registerMicroApps.ts
+++ src/registerMicroApps.ts
@@ -1,4 +1,4 @@
-import { registerMicroApps, start } from "qiankun"
+import { registerMicroApps, start, initGlobalState } from "qiankun"
 
 export const container = 'container'
 
@@ -6,11 +6,31 @@ interface ISubapps {
   name: string; // 子应用名称,注意: 唯一性
   entry: string; // 子应用入口 eg: '//localhost:3011'
   activeRule: string; // 路由匹配,需要与子应用的basename相同 eg: '/react-app'
+  props: any; // 子应用props
 }
 
 interface IApps extends ISubapps{
   container: string;
 }
+
+const initialState = {
+  user: { name: 'John Doe', age: 30 },
+  token: 'abcdefg',
+};
+
+const actions = initGlobalState(initialState);
+
+// 模拟数据变化
+setTimeout(() => {
+  actions.setGlobalState({
+    user: { name: 'lsq', age: 29 },
+    token: 'dflgjh',
+  });
+}, 5000)
+
+actions.onGlobalStateChange((state, prev) => {
+  console.log('主应用监听到全局状态变化:',state, prev);
+});
 
 // 添加子应用在这里添加即可
 const subapps: ISubapps[] = [
@@ -18,8 +38,12 @@ const subapps: ISubapps[] = [
     name: 'react-app',
     activeRule: '/react-app',
     entry: '//localhost:3011',
+    props: {
+      globalState: actions,
+    }
   }
 ]
+
 
 
 // 根据名称生成apps
