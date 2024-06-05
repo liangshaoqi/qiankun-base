e294cefe2cc0d1479057db8563c895d39c681439
Shaoqi Liang <918872153@qq.com>
2024-06-05 09:21:19 +0800

    feat 新增tabs

--- src/components/Tabs/index.tsx
+++ src/components/Tabs/index.tsx
@@ -1,3 +1,4 @@
+import { Tabs } from 'antd';
 import React, { useState, useEffect } from 'react';
 
 interface Props {
@@ -8,14 +9,35 @@ const TabsComponent: React.FC<Props> = (props) => {
 
   const {} = props
 
-  const [] = useState()
+  const [items, setItems] = useState([
+    {
+      label: 'Tab 1',
+      key: '1',
+    },
+    {
+      label: 'Tab 2',
+      key: '2',
+    },
+    {
+      label: 'Tab 3',
+      key: '3',
+    },
+    {
+      label: 'Tab 4',
+      key: '4',
+    },
+  ])
 
   useEffect(() => {}, [])
 
   return (
-    <div>
+    <Tabs
+      hideAdd
+      type="editable-card"
+      items={items}
+    >
       标签页
-    </div>
+    </Tabs>
   );
 };
 
